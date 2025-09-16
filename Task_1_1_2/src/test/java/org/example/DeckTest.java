dbae1357e36da085fccb2099c893ee52b20048a5
EvelinaVinnikova <evelinavinnikova@gmail.com>
2025-09-16 23:59:05 +0700

    The last step

--- Task_1_1_2/src/test/java/org/example/DeckTest.java
+++ Task_1_1_2/src/test/java/org/example/DeckTest.java
@@ -1,7 +1,7 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
