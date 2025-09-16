5774456d8a06c83bfbb6eb6aa9c037024895c5ea
EvelinaVinnikova <evelinavinnikova@gmail.com>
2025-09-16 22:54:42 +0700

    Fixing the order of imports_4

--- Task_1_1_1/src/test/java/org/example/HeapSortTest.java
+++ Task_1_1_1/src/test/java/org/example/HeapSortTest.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.Arrays;
 import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class HeapSortTest {
 
