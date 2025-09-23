322d3c8dd70dc08ef465655a3265fee614b1dfe2
Evanderer <evelinavinnikova@gmail.com>
2025-09-23 20:16:48 +0700

    Probably the last one

--- Task_1_1_2/src/main/java/org/example/RoundResult.java
+++ Task_1_1_2/src/main/java/org/example/RoundResult.java
@@ -1,5 +1,10 @@
 package org.example;
 
+/**
+ * Outcomes of a single Blackjack round.
+ *
+ * <p>Returned by the game logic to describe the terminal state of the round.</p>
+ */
 public enum RoundResult {
     PLAYER_WINS,
     DEALER_WINS,
