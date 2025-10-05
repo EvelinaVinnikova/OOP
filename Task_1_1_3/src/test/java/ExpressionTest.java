bb66bcab6691cc2ed4e5dc017d72000a6db2ecb8
Evanderer <evelinavinnikova@gmail.com>
2025-10-05 16:21:41 +0700

    Add test

---
+++ Task_1_1_3/src/test/java/ExpressionTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.example.Expression;
+import org.example.Variable;
+import org.junit.jupiter.api.Test;
+
+public class ExpressionTest {
+    @Test
+    void testEval_ThrowsExceptionForUndefinedVariable() {
+        // Создаем выражение с переменной "y"
+        Expression e = new Variable("y");
+        assertThrows(IllegalArgumentException.class, () -> {e.eval("x=10.0");});
+    }
+}
